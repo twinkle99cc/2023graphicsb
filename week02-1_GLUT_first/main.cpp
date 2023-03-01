d57319fb5285715b681c5455ae61a0be7cb5c669
twinkle99cc <91173732+twinkle99cc@users.noreply.github.com>
2023-03-01 22:03:59 +0800

    Add files via upload

--- week02-1_GLUT_first/main.cpp
+++ week02-1_GLUT_first/main.cpp
@@ -1,15 +1,23 @@
-#include <GL/glut.h>
+#include <GL/freeglut.h>
 void display()
 {
-    glutSolidTeapot( 0.3 );
-    glutSwapBuffers();
-}
+    glColor3f(1,1,0);///色彩 黃色
+    glutSolidTeapot(0.3);
 
-int main(int argc, char *argv[])
+    glColor3f(0,1,0);
+    glutSolidTeapot( 0.3);
+
+    glutSwapBuffers(); ///交換顯示出來
+}
+int main(int argc,char *argv[])
 {
-glutInit(&argc, argv);
-glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
-glutCreateWindow("GLUT Shapes");
-glutDisplayFunc(display);
-glutMainLoop();
+	glutInit(&argc, argv);///要開始GLUT的功能
+    glutInitWindowSize(640,480);
+    glutInitWindowPosition(10,10);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+	///要設定GLUT的顯示模式
+    glutCreateWindow("week02");///建一個視窗,叫"..."
+	glutDisplayFunc(display);///要顯示的函式
+
+	glutMainLoop();///最後要有glutMainLoop(),壓在迴圈後面
 }
