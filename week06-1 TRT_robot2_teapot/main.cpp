12d437e8283c563adf3e3709b5da9e652d035e6d
unknown <qaz.92622@gmail.com>
2023-03-23 16:19:01 +0800

    week06

---
+++ week06-1 TRT_robot2_teapot/main.cpp
@@ -0,0 +1,28 @@
+#include <GL/freeglut.h>
+float angle=0;
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glutSolidSphere(0.01,30,30);
+
+    glPushMatrix();
+        glTranslatef(0.5,0.5,0);///(3)
+        glRotatef(angle,0,0,1);///(2)
+        glTranslatef(0.45,0,0);///(1)
+        glutSolidTeapot(0.3);
+    glPopMatrix();
+
+    glutSwapBuffers();
+    angle++;
+}
+int main(int argc,char *argv[])
+{
+	glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+    glutCreateWindow("week05");
+
+	glutDisplayFunc(display);
+	glutIdleFunc(display);
+	glutMainLoop();
+}
